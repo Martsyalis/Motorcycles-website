5e952f198636bcb12a00e758ecf62ae5aeea1716
Maryussuperboy <martsyalis@gmail.com>
2017-08-24 21:56:44 -0700

    everything but css and results html page

--- js/data.js
+++ js/data.js
@@ -1,15 +1,167 @@
 
 
 
-var motorcycles = [
+var data = {};
+
+data.motorcycles = [
   {
     id: "sv650",
     title: "Suzuki sv650",
     type: "Standard",
+    typeValue: "high",
     size: "650cc",
-    height: "tall",
-    price: "6500$",
-    speed: "high",
+    sizeValue: "medium",
+    height: "medium",
+    heightValue: "medium",
+    price: "7000$",
+    priceValue: "medium",
+    Speed: "0 to 60mph in 3.3s",
+    speedValue: "high",
     image: "./assets/pics/sv.jpg"
+  },
+
+  {
+    id: "ninja300",
+    title:  "Kawasaki Ninja 300",
+    type: "sport",
+    typeValue: "medium",
+    size: "300cc",
+    sizeValue: "low",
+    height: "low",
+    heightValue:"low",
+    price: "5700$",
+    priceVlaue: "low",
+    speed: "0 to 60mph in 5.6s",
+    speedValue: "low",
+    image: "./assets/pics/ninja300.jpg"
+
+  },
+
+  {
+    id: "klr650",
+    title:  "Kawasaki KLR 650",
+    type: "Adventure",
+    typeValue: "low",
+    size: "650cc",
+    sizeValue: "medium",
+    height: "high",
+    heightValue:"high",
+    price: "6500$",
+    priceVlaue: "medium",
+    speed: "0 to 60mph in 5.4s",
+    speedValue: "low",
+    image: "./assets/pics/klr650.jpg"
+
+  },
+
+  {
+    id: "v-strom",
+    title:  "Suzuki V-Strom 650",
+    type: "Adventure",
+    typeValue: "low",
+    size: "650cc",
+    sizeValue: "medium",
+    height: "high",
+    heightValue:"high",
+    price: "8400$",
+    priceVlaue: "high",
+    speed: "0 to 60mph in 3.3s",
+    speedValue: "high",
+    image: "./assets/pics/v-strom.jpg"
+
+  },
+
+  {
+    id: "triumph-t120",
+    title:  "Triumph Bonneville T100",
+    type: "Standard",
+    typeValue: "high",
+    size: "1200cc",
+    sizeValue: "high",
+    height: "medium",
+    heightValue:"medium",
+    price: "11500$",
+    priceVlaue: "high",
+    speed: "0 to 60mph in 3.8s",
+    speedValue: "medium",
+    image: "./assets/pics/bonneville.jpg"
+
+  },
+
+  {
+    id: "triumph-triple",
+    title:  "Triumph Speed Triple",
+    type: "Sport",
+    typeValue: "medium",
+    size: "1050cc",
+    sizeValue: "high",
+    height: "medium",
+    heightValue:"medium",
+    price: "14900$",
+    priceVlaue: "high",
+    speed: "0 to 60mph in 2.7s",
+    speedValue: "high",
+    image: "./assets/pics/triple.jpg"
+
+  },
+
+  {
+    id: "yamaha-fz07",
+    title:  "Yamaha FZ-07",
+    type: "Standard",
+    typeValue: "medium",
+    size: "689cc",
+    sizeValue: "medium",
+    height: "low",
+    heightValue:"low",
+    price: "7000$",
+    priceVlaue: "medium",
+    speed: "0 to 60mph in 3.4s",
+    speedValue: "high",
+    image: "./assets/pics/fz07.jpg"
+
+  },
+
+{
+  id: "yamaha-yzf-r1",
+  title:  "Yamaha YZF R1",
+  type: "sport",
+  typeValue: "high",
+  size: "998cc",
+  sizeValue: "high",
+  height: "medium",
+  heightValue:"medium",
+  price: "15000$",
+  priceVlaue: "high",
+  speed: "0 to 60mph in 2.6s",
+  speedValue: "high",
+  image: "./assets/pics/r1.jpg"
+},
+
+];
+
+
+data.questions = [
+  {
+    key: "typeValue",
+    weight: 7
+
+  },
+  {
+    key: "sizeValue",
+    weight: 4
+  },
+  {
+    key: "heightValue",
+    weight: 2
+  },
+  {
+    key: "priceValue",
+    weight: 4
+  },
+  {
+    key: "speedValue",
+    weight: 3
   }
+
 ];
--- js/results.js
+++ js/results.js
@@ -1,3 +1,19 @@
 
-var map = localStorage.getItem("answersArray");
-console.log(map)
+var answersArray = localStorage.getItem("answersString");
+var answersArray  = answersArray.split(',');
+console.log(answersArray);
+
+var ratings = data.motorcycles.map(function(bike){
+  var rating = 0;
+  data.questions.forEach(function(question, i) {
+    var questionKeyString = question.key;
+    var questionWeight = question.weight;
+    var bikeString = bike[questionKeyString];
+    var userAnswerString = answersArray[i];
+    if( userAnswerString === bikeString){
+      rating += questionWeight;
+    }
+  })
+  return rating;
+});
+console.log(ratings);
